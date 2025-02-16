739dc6e8369197384d99a9c27fc0087022837c1d
Abdulrahman <abd3lra7man.078@gmail.com>
2025-02-16 22:14:07 +0300

    create footer, add a layout component to all pages.

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [
+    RouterLink,
+  ],
   templateUrl: './footer.component.html',
   styles: ``
 })
 export class FooterComponent {
-
 }
